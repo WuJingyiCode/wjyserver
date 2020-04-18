159fa16419ee1e654f623f362c0bd76308b901b8
WuJingyiCode <38290112+WuJingyiCode@users.noreply.github.com>
2020-04-18 18:11:05 +0800

    【WJYServer】DemoExtension测试

--- src/main/java/com/yi/server/wjyserver/extension/DemoExtension.java
+++ src/main/java/com/yi/server/wjyserver/extension/DemoExtension.java
@@ -1,10 +1,21 @@
 package com.yi.server.wjyserver.extension;
 
+import com.yi.server.wjyserver.annotation.RequestListener;
+import message.ProtobufMessage;
+
 /**
  * @author Cakutau
  */
 public class DemoExtension extends Extension {
+    private static final String CMD_TEST = "";
+
     public DemoExtension(int id) {
         super(id);
+    }
+
+    @RequestListener(CMD_TEST)
+    public void test() {
+        ProtobufMessage message = ProtobufMessage.create();
+        message.putInt("", 1);
     }
 }
