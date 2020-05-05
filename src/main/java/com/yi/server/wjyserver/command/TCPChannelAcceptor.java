f906a12ce9667bee5e869b29fefcb1cfd47b414e
WuJingyiCode <38290112+WuJingyiCode@users.noreply.github.com>
2020-05-05 15:29:37 +0800

    【WJYServer】另起线程

--- src/main/java/com/yi/server/wjyserver/command/TCPChannelAcceptor.java
+++ src/main/java/com/yi/server/wjyserver/command/TCPChannelAcceptor.java
@@ -8,6 +8,7 @@ import java.net.ServerSocket;
 import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.Executors;
 
 /**
  * 用于接收TCP连接。
@@ -21,6 +22,7 @@ public class TCPChannelAcceptor extends Command {
     private Selector selector;
     private SessionManager sessionManager;
     private boolean isRunning;
+    private Thread acceptorThread;
 
     public TCPChannelAcceptor(WJYServer server) throws IOException {
         serverSocketChannel = ServerSocketChannel.open();
@@ -30,6 +32,8 @@ public class TCPChannelAcceptor extends Command {
         selector = Selector.open();
         this.server = server;
         sessionManager = server.getSessionManager();
+        // todo 改用线程池
+        acceptorThread = new Thread(new AcceptorRunnable(), "Thread-TCPChannelAcceptor");
     }
 
     @Override
@@ -38,22 +42,35 @@ public class TCPChannelAcceptor extends Command {
 
         // todo 如何定义running状态？
         while (isRunning) {
-            int count = selector.select();
-            if (count > 0) {
-                Set<SelectionKey> selectionKeys = selector.selectedKeys();
-                Iterator<SelectionKey> iterator = selectionKeys.iterator();
-                while (iterator.hasNext()) {
-                    SelectionKey selectionKey = iterator.next();
-                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
-                    SocketChannel socketChannel = serverSocketChannel.accept();
-                    sessionManager.addNewSocketChannel(socketChannel);
-                }
-            }
+            acceptorThread.start();
         }
     }
 
     @Override
     protected void rollbackOnce() {
 
+    }
+
+    private class AcceptorRunnable implements Runnable {
+        @Override
+        public void run() {
+            while (isRunning) {
+                try {
+                    int count = selector.select();
+                    if (count > 0) {
+                        Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                        Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                        while (iterator.hasNext()) {
+                            SelectionKey selectionKey = iterator.next();
+                            ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+                            SocketChannel socketChannel = serverSocketChannel.accept();
+                            sessionManager.addNewSocketChannel(socketChannel);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
